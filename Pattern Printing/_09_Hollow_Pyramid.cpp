962cbea323c69b5bd26140099345e2ac03f8d7f4
lochan-gosavi <lochangosavi05@gmail.com>
2026-08-10 18:07:43 +0530

    first

--- Pattern Printing/_09_Hollow_Pyramid.cpp
+++ Pattern Printing/_09_Hollow_Pyramid.cpp
@@ -4,18 +4,7 @@ int main()
 {
     int n;
     cin >> n;
-    // for (int i = 0; i < n; i++)
-    // {
-    //     for (int j = 1; j <= n - i - 1; j++)
-    //     {
-    //         cout << " ";
-    //     }
-    //     for (int j = 1; j <= i + 1; j++)
-    //     {
-    //         cout << "*";
-    //     }
-    //     cout << endl;
-    // }
+
     for (int i = 0; i < n; i++)
     {
         for (int j = 1; j <= n; j++)
